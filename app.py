68c7afba88fb40e1aaa5e14f0c44931dca6a6a04
larak <larak@LAPTOP-TMUI1PP4>
2022-07-14 01:08:21 +0200

    add calendar

--- app.py
+++ app.py
@@ -22,7 +22,22 @@ def home():
         description="Organisiere deinen Arbeitstag mit Workday.",
     )
 
-
+@app.route("/calendar", methods=['GET', 'POST'])
+def calendar():
+    """Landing page route."""
+    nav = [
+        {"name": "Dein Tag", "url": "https://example.com/1", "iconName": "home"},
+        {"name": "Kalender", "url": "https://example.com/2", "iconName": "calendar_month"},
+        {"name": "Spaces", "url": "https://example.com/4", "iconName": "search"},
+        {"name": "Buchungen", "url": "https://example.com/5", "iconName": "inventory"},
+        {"name": "Profil", "url": "https://example.com/6", "iconName": "account_circle"},
+    ]
+    return render_template(
+        "calendar.html",
+        nav=nav,
+        title="workday",
+        description="Organisiere deinen Arbeitstag mit Workday.",
+    )
 
 if __name__ == "__main__":
     app.run()
